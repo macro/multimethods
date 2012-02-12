afc062ef4ebb1c80265872cdca12362bbf74eac8
Neil Chintomby <neil@mochimedia.com>
2012-02-11 16:53:35 -0800

    version bump

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from distutils.core import setup
 
 setup(
     name         = 'multimethods',
-    version      = '0.1',
+    version      = '0.2',
     description  = 'Clojure-style multimethods for Python',
     author       = 'Daniel Werner',
-    url          = 'http://github.com/danwerner/multimethods',
+    url          = 'http://github.com/macro/multimethods',
     py_modules   = ['multimethods'],
     entry_points = {},
 )
